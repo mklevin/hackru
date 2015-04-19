1fdb92cdba5636075893563b98375de4672f64eb
jer-keel <jj.of.keeler@gmail.com>
2015-04-19 00:04:06 -0400

    add googleid

--- src/backend/routes/api.js
+++ src/backend/routes/api.js
@@ -12,7 +12,7 @@ var Company = require("../models/Company");
 
 // ___________________users___________________
 // Return all users
-router.get("/users", function(req, res, next) {
+router.get("/users", loggedIn, function(req, res, next) {
   users = [];
   Searcher.find(function(err, searchers) {
     if (err) next(err);
@@ -27,7 +27,7 @@ router.get("/users", function(req, res, next) {
 });
 
 // Return a specific user based on their mongodb id
-router.get("/users/id/:id", function(req, res, next) {
+router.get("/users/id/:id", loggedIn, function(req, res, next) {
   Searcher.findById(req.params.id, function(err, searcher) {
     if (err) next(err);
     res.json(searcher);
@@ -42,7 +42,7 @@ router.get("/users/id/:id", function(req, res, next) {
 
 // ___________________companies___________________
 // Return all companies
-router.get("/companies", function(req, res, next) {
+router.get("/companies", loggedIn, function(req, res, next) {
   Company.find(function(err, companies) {
     if (err) next(err);
     res.json(companies);
@@ -50,7 +50,7 @@ router.get("/companies", function(req, res, next) {
 });
 
 // Return a specific company based on their mongodb id
-router.get("/companies/id/:id", function(req, res, next) {
+router.get("/companies/id/:id", loggedIn, function(req, res, next) {
   Company.findById(req.params.id, function(err, comapny) {
     if (err) next(err);
     res.json(company);
@@ -59,7 +59,7 @@ router.get("/companies/id/:id", function(req, res, next) {
 
 // ___________________listings___________________
 // return all listings
-router.get("/listings", function(req, res, next) {
+router.get("/listings", loggedIn, function(req, res, next) {
   params = {
     number: 10,
     type: "internship",
@@ -104,7 +104,8 @@ router.get("/listings/id/:id", function(req, res, next) {
 
 // ___________________users___________________
 // Create a new user, either a searcher or employer
-router.post("/users", function(req, res, next) {
+router.post("/users", loggedIn, function(req, res, next) {
+  req.body.googleid = req.user.id;
   if (req.body.type === "searcher") {
     Searcher.create(req.body, function(err, searcher) {
       if (err) next(err);
@@ -119,7 +120,7 @@ router.post("/users", function(req, res, next) {
 });
 
 // Update the resume of a user
-router.post("/users/id/:id/resume", function(req, res, next) {
+router.post("/users/id/:id/resume", loggedIn, function(req, res, next) {
   Searcher.findById(req.params.id, function(err, searcher) {
     if (err) next(err);
     if (searcher.type === "searcher") {
@@ -130,7 +131,7 @@ router.post("/users/id/:id/resume", function(req, res, next) {
 });
 
 // Update the search preferences of the user
-router.post("/users/id/:id/searchprefs", function(req, res, next) {
+router.post("/users/id/:id/searchprefs", loggedIn, function(req, res, next) {
   Searcher.findById(req.params.id, function(err, searcher) {
     if (err) next(err);
     if (searcher.type === "searcher") {
@@ -142,7 +143,7 @@ router.post("/users/id/:id/searchprefs", function(req, res, next) {
 
 // ___________________companies___________________
 // Create a new company
-router.post("/companies", function(req, res, next) {
+router.post("/companies", loggedIn, function(req, res, next) {
   Company.create(req.body, function(err, company) {
     if (err) next(err);
     res.json(company);
@@ -151,7 +152,7 @@ router.post("/companies", function(req, res, next) {
 
 // ___________________listings___________________
 // Create a new listing
-router.post("/listings", function(req, res, next) {
+router.post("/listings", loggedIn, function(req, res, next) {
   Listing.create(req.body, function(err, listing) {
     if (err) next(err);
     res.json(listing);
@@ -163,7 +164,7 @@ router.post("/listings", function(req, res, next) {
 // =============================================================================
 
 // ___________________users___________________
-router.delete("/users/id/:id", function(req, res, next) {
+router.delete("/users/id/:id", loggedIn, function(req, res, next) {
   Searcher.findByIdAndRemove(req.params.id, req.body, function(err, searcher) {
     if (err) next(err);
     if (searcher) res.json(searcher);
@@ -175,7 +176,7 @@ router.delete("/users/id/:id", function(req, res, next) {
 });
 
 // ___________________companies___________________
-router.delete("/companies/id/:id", function(req, res, next) {
+router.delete("/companies/id/:id", loggedIn, function(req, res, next) {
   Company.findByIdAndRemove(req.params.id, req.body, function(err, company) {
     if (err) next(err);
     res.json(company);
@@ -183,11 +184,21 @@ router.delete("/companies/id/:id", function(req, res, next) {
 });
 
 // ___________________listings___________________
-router.delete("/listings/id/:id", function(req, res, next) {
-  Company.findByIdAndRemove(req.params.id, req.body, function(err, listing) {
+router.delete("/listings/id/:id", loggedIn, function(req, res, next) {
+  Listing.findByIdAndRemove(req.params.id, req.body, function(err, listing) {
     if (err) next(err);
     res.json(listing);
   });
 });
+
+// Check to see if a user is logged in, if not, redirect them
+function loggedIn(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    console.log("not logged in");
+    res.redirect("/");
+  }
+}
 
 module.exports = router;
